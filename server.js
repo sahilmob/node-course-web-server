eed413ccd61a3987fdfd47106a3e79cf0341ed60
root <root@DESKTOP-COMGLI5.localdomain>
2018-06-05 00:15:33 +0300

    Commenting a middleware that blocks app from running

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
-app.use((req, res, next) => {
-    res.render('maintinance.hbs')
-})
+// app.use((req, res, next) => {
+//     res.render('maintinance.hbs')
+// })
 
 app.use((req, res, next) => {
     var now = new Date().toString();
